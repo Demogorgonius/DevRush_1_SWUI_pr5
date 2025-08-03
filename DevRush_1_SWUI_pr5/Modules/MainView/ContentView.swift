bca11f586efee6449ec33ba1d052cacf7926c289
Demogorgonius <51359795+Demogorgonius@users.noreply.github.com>
2025-08-03 10:22:43 +0300

    Create project struct

---
+++ DevRush_1_SWUI_pr5/Modules/MainView/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  DevRush_1_SWUI_pr5
+//
+//  Created by Sergey on 02.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord: String = ""
+    @State private var newWord: String = ""
+    
+    
+    
+    var body: some View {
+
+        NavigationStack {
+            List {
+                Section {
+                    
+                }
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
